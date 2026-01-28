70d9593b79ebfd4c257eea4d1ef94460fbd5622b
tjddn88 <76651042+tjddn88@users.noreply.github.com>
2026-01-28 23:11:05 +0900

    fix: 카테고리 칩 가로 스크롤 동작 수정

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -58,9 +58,9 @@ export default function FilterButtons({ selected, onChange, selectedGrades, onGr
   const isFiltered = selectedGrades.size < 3;
 
   return (
-    <div className="flex items-center relative">
+    <div className="flex items-center relative overflow-hidden">
       {/* 스크롤 가능한 카테고리 칩 영역 */}
-      <div className="flex-1 overflow-x-auto scrollbar-hide">
+      <div className="flex-1 min-w-0 overflow-x-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
         <div className="flex gap-2 pr-14 py-1">
           {FILTERS.map(({ type, label, emoji }) => (
             <button
